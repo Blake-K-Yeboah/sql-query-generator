7f7895399707a9c1707ab5a5c53db2d26c31e9b8
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-02 23:17:30 +1000

    Added Query Box and Clear Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [query, setQuery] = useState('');
+
+  const clearQuery = () => {
+    setQuery('');
+  }
+
   return (
+
     <div className="app">
+
       <h1 className="page-title"><span>SQL</span> Query Generator</h1>
+
+      <div className="query-box">
+
+        <span className="query" dangerouslySetInnerHTML={{__html: query || "(Blank)"}}></span>
+
+        <button className="btn primary" onClick={clearQuery}>Clear</button>
+
+      </div>
+
     </div>
+
   );
+
 }
 
 export default App;
