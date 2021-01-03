d6674161d0b2db24c4c7dc8d06311c9177deccb5
Blake Yeboah <blakekyeboah@gmail.com>
2021-01-03 16:51:33 +1000

    Added Type of Query Selector

--- src/App.js
+++ src/App.js
@@ -9,19 +9,48 @@ function App() {
     setQuery('');
   }
 
+  const [queryType, setQueryType] = useState(null);
+
+  const typeOfQueryHandler = (type) => {
+    setQueryType(type);
+
+    switch(type) {
+      case 'select':
+        setQuery("<b>SELECT</b> * <b>FROM</b> `table_name`");
+    }
+  }
+
   return (
 
     <div className="app">
 
       <h1 className="page-title"><span>SQL</span> Query Generator</h1>
 
-      <div className="query-box">
+      <section className="query-box">
 
-        <span className="query" dangerouslySetInnerHTML={{__html: query || "(Blank)"}}></span>
+        <span className="query" dangerouslySetInnerHTML={{__html: query || "<i>Blank</i>"}}></span>
 
         <button className="btn primary" onClick={clearQuery}>Clear</button>
 
-      </div>
+      </section>
+
+      <section className="option-selector">
+
+        <h3 className="option-title">Type of query: </h3>
+
+        <section className="option-btn-group">
+
+          <button className={`btn ${queryType === 'select' ? 'primary' : 'primary-outline'}`} onClick={typeOfQueryHandler.bind(this, 'select')}>
+            SELECT
+          </button>
+
+          <button className={`btn ${queryType === 'delete' ? 'primary' : 'primary-outline'}`} onClick={typeOfQueryHandler.bind(this, 'delete')}>
+            DELETE
+          </button>
+
+        </section>
+
+      </section>
 
     </div>
 
